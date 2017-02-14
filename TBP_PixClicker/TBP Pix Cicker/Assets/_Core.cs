de3745d401e4e35f73b9986f66879bcaaa36b179
xennis271 <gxennis20@gmail.com>
2017-02-14 12:42:32 -0800

    Made the base game
    The new Clicker!

---
+++ TBP_PixClicker/TBP Pix Cicker/Assets/_Core.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class _Core : MonoBehaviour {
+	[Header("Stats LIVE")]
+	public int Gold;
+	public Slider Hp; // gets rounded
+	public int Lv;
+	public int Zone;
+	public int Deaths;
+	public Slider EXPbar;
+	public int AmountWeTook = 0;
+	[Header("GO's")]
+	public Slider Bar;
+	public GameObject Bad;
+	public GameObject StartGO;
+	public GameObject GameStart;
+	[Header("List")]
+	public List<Sprite> MobsNormal = new List<Sprite>();
+	public List<Sprite> MobsAttack = new List<Sprite>();
+	public List<Sprite> MobsHurt = new List<Sprite>();
+	public List<Sprite> MobsAbl = new List<Sprite>();
+	public List<string> MobAbls = new List<string> ();
+	[Header("UI stuff")]
+	public GameObject ShopGO;
+	public List<GameObject> Pages = new List<GameObject> ();
+	public int PageNum;
+	[Header("Text for stats")]
+	public Text GoldT;
+	public Text HpT;
+	public Text LvT;
+	public Text ZoneT;
+	public Text DeathsT;
+
+
+	public void Removehp(float amount){
+		Bar.value = Bar.value - amount;
+	
+	}
+	public void Heal() {
+		Bar.value = Bar.maxValue;
+	}
+
+	public void SummonNewBad() {
+		Bad.SetActive (false);
+		// update setings...
+		int outp;
+		outp = Random.Range(0,MobsNormal.Count);
+		//Debug.Log (outp);
+		Bad.GetComponent<badguy> ().NormalPic = MobsNormal [outp];
+		Bad.GetComponent<badguy> ().AttackingPic= MobsAttack [outp];
+		Bad.GetComponent<badguy> ().DamagedPic = MobsHurt [outp];
+		Bad.GetComponent<badguy> ().AblPic = MobsAbl [outp];
+		string outs = "";
+		outs = MobAbls[outp];
+		if (outs == "Invis") {
+			Bad.GetComponent<badguy> ().Invs = true;
+		} else {
+			Bad.GetComponent<badguy> ().Invs = false;
+		}
+		if (outs == "Rage") {
+			Bad.GetComponent<badguy> ().Rage = true;
+		} else {
+			Bad.GetComponent<badguy> ().Rage = false;
+		}
+		if (outs == "Pos") {
+			Bad.GetComponent<badguy> ().Pos = true;
+		} else {
+			Bad.GetComponent<badguy> ().Pos = false;
+		}
+		Bad.GetComponent<badguy> ().BadHpBar.value = Bad.GetComponent<badguy> ().BadHpBar.maxValue;
+		Bad.GetComponent<badguy> ().Damage = 1;
+		Bad.SetActive (true);
+	}
+	public void OpenShop() {
+	// need to summon a new dude after closing it
+		PageNum = -1; // the list starts @ 0
+		ShopGO.SetActive (true);
+	}
+	public void CloseShop(){
+		PageNum = -1; // the list starts at 0
+		ShopGO.SetActive (false);
+	}
+	public void UpdatePages() {
+		// we need to close all pages...
+		foreach (GameObject Page in Pages) {
+			Page.SetActive (false);
+		}
+		// open the one we want.
+		Pages [PageNum].SetActive (true);
+	}
+	public void TurnPage() {
+		PageNum++;
+		UpdatePages ();
+	}
+	public void TurnBackAPage() {
+		PageNum--;
+		UpdatePages ();
+	}
+	public void UpdateStats() {
+		GoldT.text = Gold.ToString();
+		HpT.text = Mathf.RoundToInt(Hp.value).ToString(); // looks better!
+		LvT.text = Lv.ToString();
+		ZoneT.text = Zone.ToString();
+		DeathsT.text = Deaths.ToString();
+		if (Hp.value <= 0) { // adds to the death pool but no math yet...
+		
+			Hp.value = 100;
+			HpT.text = Hp.ToString();
+			Deaths++;
+			DeathsT.text = Deaths.ToString();
+			// Money and math time!
+
+			AmountWeTook = (AmountWeTook*2) + (Deaths*10);
+			//Debug("Amount you had" + Gold.ToString() + " The amount we took" +AmountWeTook.ToString()); 
+			Gold = Gold - AmountWeTook; // << thats one mean mean line!
+			if (Gold < 0) {
+				Gold = 0;
+			}
+		
+		}
+		if(EXPbar.value == EXPbar.maxValue){
+			EXPbar.value = EXPbar.minValue;
+			Lv++;
+			EXPbar.maxValue = EXPbar.maxValue * 2; // trying to grow!
+		}
+	}
+	int PreZone;
+	public void KilledMob() {
+		Zone++;
+		PreZone++;
+		Gold = Gold + Zone * Zone / 2;
+		// Nice job!
+		// need to give some EXP
+		EXPbar.value = EXPbar.value + Zone/2;
+		if (PreZone == 10) {
+			EXPbar.maxValue = Zone * 2.5f + EXPbar.maxValue;
+		}
+	}
+	public void Update(){
+		UpdateStats ();
+	}
+	public void Start(){
+		AmountWeTook = 1;
+		StartGO.SetActive (true);
+		GameStart.SetActive (false);
+	}
+	public void StartGame() {
+		GameStart.SetActive (true);
+		StartGO.SetActive (false);
+	}
+}
---
+++ TBP_PixClicker/TBP Pix Cicker/Assets/badguy.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class badguy : MonoBehaviour {
+	[Header("API's")]
+	public GameObject _Core;
+	[Header("pics and things")]
+	public Slider BadHpBar;
+	public Button but;
+	public Image Hold;
+	public Sprite DamagedPic;
+	public Sprite NormalPic;
+	public Sprite AblPic;
+	public Sprite AttackingPic;
+	public int DamPerHit;
+	public int Damage;
+	[Header("Times and things")]
+	public int t = 0;
+	public int t2 = 0;
+	public int t3 = 0;
+	public int TimeTellAttack;
+	public int maxTime;
+	public bool Posed = false;
+	[Header("Abilty(s)")]
+	public bool Invs = true;
+	public bool Rage = false;
+	public bool Pos = false;
+	public int PosTime = 500;
+
+	IEnumerator damw()
+	{
+		yield return new WaitForSeconds(1); // add a little bit of time for the user to read the last text!
+		//Hold.sprite = NormalPic;
+	
+	}
+	IEnumerator Abl()
+	{
+		// you cant hit during abl
+		if (Invs) {
+			but.interactable = false;
+		}
+		if (Rage && Damage <= 3) {
+			Damage++;
+		}
+		if (Pos) {
+		// apply pos
+			Posed = true;
+		}
+		Hold.sprite = AblPic;
+		yield return new WaitForSeconds(5); // add a little bit of time for the user to read the last text!
+		if (Invs) {
+			but.interactable = true;
+		}
+		if (Rage || Damage > 2) {
+			Rage = false;
+		}
+		Hold.sprite = NormalPic;
+		t = 0;
+	}
+	IEnumerator Attack()
+	{
+		_Core.GetComponent<_Core> ().Removehp (Damage);
+		// you cant hit during abl
+		//but.interactable = false;
+		Hold.sprite = AttackingPic;
+		yield return new WaitForSeconds(1); // add a little bit of time for the user to read the last text!
+		//but.interactable = true;
+		Hold.sprite = NormalPic;
+
+
+	}
+	public void dam() {
+		BadHpBar.value = BadHpBar.value - DamPerHit;
+		Hold.sprite = DamagedPic;
+		StartCoroutine ("damw");
+
+	}
+	public void Update() {
+		t++;
+		t++;
+		t2++;
+		//t2++;
+		if (Posed) {
+			t3++;
+			_Core.GetComponent<_Core> ().Removehp (0.1f * ((Mathf.RoundToInt(_Core.GetComponent<_Core> ().Zone/4f))/4));
+			if (t3 >= PosTime) {
+				t3 = 0;
+				Posed = false;
+			}
+		}
+		if (BadHpBar.value == BadHpBar.minValue) {
+			// so the _core handles almost all of this so lets use the api built in.
+			_Core.GetComponent<_Core> ().SummonNewBad ();
+			_Core.GetComponent<_Core>().KilledMob();
+			_Core.GetComponent<_Core> ().UpdateStats ();
+			Damage = Damage + Mathf.RoundToInt(_Core.GetComponent<_Core> ().Zone/4f);
+		}
+		if (t >= maxTime) {
+			// run ablit
+			but.interactable = true;
+			Hold.sprite = AblPic;
+			StartCoroutine("Abl");
+
+		}
+		if (t2 >= TimeTellAttack) {
+		
+			t2 = 0;
+			StartCoroutine ("Attack");
+
+			}
+	}
+
+}

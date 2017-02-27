a352f00941dcf6913c33f21355d4b033be48932b
xennis271 <gxennis20@gmail.com>
2017-02-27 05:25:38 -0600

    Ads!
    We have added a add to the "run" button in unity as well as added the
    app to the playstore!

---
+++ TBP_PixClicker/TBP Pix Cicker/Assets/_Ads.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class UnityAdsExample : MonoBehaviour
+{
+	public void ShowAd()
+	{
+		if (Advertisement.IsReady())
+		{
+			Advertisement.Show();
+		}
+	}
+}

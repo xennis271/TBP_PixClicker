86c79203ce5858428e63d353fb5e165a4216f525
xennis271 <gxennis20@gmail.com>
2017-03-21 12:36:11 -0500

    Revert "Bosses and Dodging"
    This reverts commit 4e422d3a6766825a8cb3d1ee37a997d0908003ae.

--- TBP_PixClicker/TBP Pix Cicker/Assets/scripts/badguy.cs
+++ TBP_PixClicker/TBP Pix Cicker/Assets/scripts/badguy.cs
@@ -6,12 +6,6 @@ using UnityEngine;
 public class badguy : MonoBehaviour {
 	[Header("API's")]
 	public GameObject _Core;
-	public GameObject _Abl;
-	[Header("TypeOfMonster")]
-	public bool Normal = true;
-	public bool Boss = false;
-	[Header("List")]
-	public List<GameObject> D_Buttons;
 	[Header("pics and things")]
 	public Slider BadHpBar;
 	public Button but;
@@ -29,24 +23,15 @@ public class badguy : MonoBehaviour {
 	public int TimeTellAttack;
 	public int maxTime;
 	public bool Posed = false;
-	[Header("Monster Abilty(s)")]
+	[Header("Abilty(s)")]
 	public bool InAbilty = true;
 	public bool Invs = true; // hidden from attacks
 	public bool Rage = false; // +1 to all attacks (adds on per rage)
 	public bool Pos = false; // applys posion to player (-0.01hp/tick)
 	public int PosTime = 500; // how long it applys the pos effect
 	public bool Healing = false; // heals the badguy (offen combo with Invs
-	public bool Flash = false; // turns the game off and back on.
-	public bool Adv_Block = false; // turns on the adv_block layor
-	public bool Scream = false; // turns off all player skills.
-	public bool Drain = false; // drains player for HP
-	[Header("Player Bools")]
-	public bool P_Invs = false;
 	//public bool Clone = false; // the idea is that there are two monsters you must kill
 	//public bool Grow = false; // the idea is that the monster hp *2/abl
-	[Header("GO's")]
-	public GameObject Game;
-	public List<GameObject> BlockScreen;
 
 	IEnumerator damw()
 	{
@@ -57,11 +42,6 @@ public class badguy : MonoBehaviour {
 	IEnumerator Abl()
 	{
 		// you cant hit during abl
-		if (Drain) { // due to drain -hp + hp /t ... we have those.
-			Drain = false;
-			Healing = true;
-			Pos = true;
-		}
 		if (Invs) {
 			but.interactable = false;
 			InAbilty = true;
@@ -78,24 +58,7 @@ public class badguy : MonoBehaviour {
 		//Hold.sprite = AblPic;
 		if (Healing == true) {
 			BadHpBar.value = BadHpBar.value + _Core.GetComponent<_Core>().Zone/100 + 0.5f;
-			// boss edit...
-			if (Boss) {
-				// if its a boss its a % of total hp.
-				BadHpBar.value = BadHpBar.value * 1.001f; // set to (0.001% + zone)/tick (realy fast tho!)
-			}
 			InAbilty = true;
-		}
-		if (Flash) {
-			// we need to kill the game and start up a white screen. for about 1s or somthing then run the game.
-			Game.SetActive(false);
-		}
-		if (Adv_Block) {
-			foreach (var block in BlockScreen) {
-				block.SetActive (true);
-			}
-		}
-		if (Scream) {
-			_Abl.GetComponent<Abl> ().LockSkills (); // turns on the cd for all skills.
 		}
 		yield return new WaitForSeconds(5); // add a little bit of time for the user to read the last text!
 		if (Invs) {
@@ -106,14 +69,6 @@ public class badguy : MonoBehaviour {
 			Rage = false;
 			InAbilty = false;
 		}
-		if (Flash) {
-			// we need to kill the game and start up a white screen. for about 1s or somthing then run the game.
-			Game.SetActive(true);
-
-		}
-
-
-
 		//Hold.sprite = AttackingPic;
 		t = 0;
 	}
@@ -143,11 +98,6 @@ public class badguy : MonoBehaviour {
 		t++;
 		t2++;
 //		AblPic = AblPic;
-		if (Boss) { // we are not a normal monster.
-			Normal = false;
-		} else {
-			Normal = true;
-		}
 		if (Posed) {
 			t3++;
 			_Core.GetComponent<_Core> ().Removehp (0.1f * ((Mathf.RoundToInt(_Core.GetComponent<_Core> ().Zone/4f))/8));
@@ -157,21 +107,15 @@ public class badguy : MonoBehaviour {
 			}
 		}
 		if (BadHpBar.value == BadHpBar.minValue) {
-			if (Normal) { // we dont handle non-normal deaths.
-				// so the _core handles almost all of this so lets use the api built in.
-				Debug.Log ("before:" + BadHpBar.maxValue);
-				BadHpBar.maxValue = BadHpBar.maxValue + _Core.GetComponent<_Core> ().Zone / 4;
-				Debug.Log ("after:" + BadHpBar.maxValue);
-				_Core.GetComponent<_Core> ().SummonNewBad ();
-				_Core.GetComponent<_Core> ().KilledMob ();
-				_Core.GetComponent<_Core> ().UpdateStats ();
-				//_Core.GetComponent<_Core> ().Zone++;
-				Damage = Damage + Mathf.RoundToInt (_Core.GetComponent<_Core> ().Zone / 8f);
-			} else {
-			// tell core that what ever is going on its dead.
-				_Core.GetComponent<_Core>().BossKilled();
-				// WE DONT SET IT BACK we just tell them its over.
-			}
+			// so the _core handles almost all of this so lets use the api built in.
+			Debug.Log("before:" + BadHpBar.maxValue);
+			BadHpBar.maxValue = BadHpBar.maxValue + _Core.GetComponent<_Core>().Zone/8;
+			Debug.Log("after:" + BadHpBar.maxValue);
+			_Core.GetComponent<_Core> ().SummonNewBad ();
+			_Core.GetComponent<_Core>().KilledMob();
+			_Core.GetComponent<_Core> ().UpdateStats ();
+			//_Core.GetComponent<_Core> ().Zone++;
+			Damage = Damage + Mathf.RoundToInt(_Core.GetComponent<_Core> ().Zone/8f);
 
 		}
 		if (t >= maxTime) {
@@ -186,18 +130,7 @@ public class badguy : MonoBehaviour {
 		
 			t2 = 0;
 			Hold.sprite = AttackingPic;
-			if (P_Invs != true) {
-				StartCoroutine ("Attack");
-				int outcome = Random.Range (0, 100);
-				if (outcome <= 25) {
-					foreach (var thing in D_Buttons) {
-						thing.SetActive (false);
-					}
-					outcome = Random.Range (0, D_Buttons.Count);
-					D_Buttons [outcome].SetActive (true);
-
-				}
-			}
+			StartCoroutine ("Attack");
 
 			}
 	}
